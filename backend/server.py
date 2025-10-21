7ed51f0fba8e660ee2b1fe2b21e206674e4cb6d2
Anis Krari <2041359-AnisK@users.noreply.replit.com>
2025-10-21 08:08:47 +0000

    Petite modif

--- backend/server.py
+++ backend/server.py
@@ -201,4 +201,7 @@ if os.getenv("REPL_ID"):
 # Run app
 # ---------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
+
